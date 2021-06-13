446cb4fc1d7b04f1eaf324eb6318e2fbe8456270
Julio Sergio <juliosergio010302@gmail.com>
2021-06-13 16:58:50 -0300

    Adding temporary icons

--- organizer/src/components/Header.jsx
+++ organizer/src/components/Header.jsx
@@ -1,12 +1,12 @@
 const Header = () => {
     return (
         <div>
-            <p><a href="#">Organizer</a></p>
+            <p><a href="will change later">Organizer</a></p>
             <nav>
                 <ul>
-                    <li><a href="https://www.linkedin.com/in/julio-sergio-ferreira-silva/">L</a></li>
-                    <li><a href="https://github.com/JulioSergioFS">G</a></li>
-                    <li><a href="https://juliosergiofs.medium.com/">M</a></li>
+                    <li><a href="https://www.linkedin.com/in/julio-sergio-ferreira-silva/"><img src="https://img-premium.flaticon.com/png/512/174/174857.png?token=exp=1623614920~hmac=0a734d732892691dce59d93b2463bda4" alt="Linkedin" /></a></li>
+                    <li><a href="https://github.com/JulioSergioFS"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" /></a></li>
+                    <li><a href="https://juliosergiofs.medium.com/"><img src="https://lh3.googleusercontent.com/proxy/79MNuyGIJZV9KLx8ngylbtLbDYXYx517QfZ2od56BG6vtmCuT9cweV-hn3h1863vUuqxdCiNGnBktV2c2W310N9IMeVkMQU86jA92vQ2tYbB6uhUHi1F8Q" alt="Medium" /></a></li>
                 </ul>
             </nav>
 
