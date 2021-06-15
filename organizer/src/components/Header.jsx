7f957f3174bf8f7cf70aefc4c0f570561ab43b5f
Julio Sergio <juliosergio010302@gmail.com>
2021-06-15 10:24:08 -0300

    Change the link of the medium logo
    The last link was broken.

--- organizer/src/components/Header.jsx
+++ organizer/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
                 <ul className='list'>
                     <li className='bubble list-item'><a href="https://www.linkedin.com/in/julio-sergio-ferreira-silva/"><img className='header-icon' src="https://img-premium.flaticon.com/png/512/174/174857.png?token=exp=1623614920~hmac=0a734d732892691dce59d93b2463bda4" alt="Linkedin" /></a></li>
                     <li className='bubble list-item'><a href="https://github.com/JulioSergioFS"><img className='header-icon' src="https://image.flaticon.com/icons/png/512/25/25231.png" alt="GitHub" /></a></li>
-                    <li className='bubble list-item'><a href="https://juliosergiofs.medium.com/"><img className='header-icon' src="https://lh3.googleusercontent.com/proxy/79MNuyGIJZV9KLx8ngylbtLbDYXYx517QfZ2od56BG6vtmCuT9cweV-hn3h1863vUuqxdCiNGnBktV2c2W310N9IMeVkMQU86jA92vQ2tYbB6uhUHi1F8Q" alt="Medium" /></a></li>
+                    <li className='bubble list-item'><a href="https://juliosergiofs.medium.com/"><img className='header-icon' src="https://cdn4.iconfinder.com/data/icons/social-media-circle-7/512/Medium_circle-512.png" alt="Medium" /></a></li>
                 </ul>
             </nav>
 
