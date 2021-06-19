d37eb5b573cc313a8541c6cb490640be7fd8aac7
Julio Sergio <juliosergio010302@gmail.com>
2021-06-19 13:14:04 -0300

    Started to use css grid with the Main Panel

--- organizer/src/components/MainPanel.jsx
+++ organizer/src/components/MainPanel.jsx
@@ -1,9 +1,14 @@
+import '../styles/components/MainPanel.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const MainPanel = () => {
     return (
         <div className='main-panel'>
-            
+            <Link to="/ideas" className='main-panel_item'>Ideas</Link>
+            <Link to="/calendar" className='main-panel_item'>Calendar</Link>
+            <Link to="/reminders" className='main-panel_item'>Reminder</Link>
+            <Link to="/to-do" className='main-panel_item'>To Do</Link>
         </div>
     )
 }
--- organizer/src/pages/Home.jsx
+++ organizer/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import '../styles/global.css'
 import '../styles/pages/Home.css'
-import { Link } from 'react-router-dom'
 import Header from '../components/Header.jsx'
 import MainPanel from '../components/MainPanel.jsx'
 
@@ -9,10 +8,6 @@ const Home = () => {
     <>
       <Header />
       <MainPanel />
-      <Link to="/ideas">Ideas</Link>
-      <Link to="/calendar">Calendar</Link>
-      <Link to="/reminders">Reminder</Link>
-      <Link to="/to-do">To Do</Link>
     </>
   );
 }
